724fc5f47625d2b3a5f896a34964b3242279277a
kketums <ssmutekk@proton.me>
2026-02-08 17:01:16 -0500

    added geolocation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use compile_dotenv::compile_env;
+use geolocation;
 use reqwest::{self, blocking};
 use serde_json::Value;
-// use std::env;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -13,37 +13,63 @@ use serde_json::Value;
 
 struct Args {
     /// ie.. CA ON TO
-    #[arg(default_values = &["CA", "ON", "TO"])]
-    area: Vec<String>,
+    #[arg()]
+    area: Option<Vec<String>>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = compile_env!("API");
+    let api_str = api_key.to_string();
 
     let args = Args::parse();
 
-    let area = args.area;
-    let country_code = &area[0];
-    let province_code = &area[1];
-    let city_code = &area[2];
+    let country_code;
+    let province_code;
+    let city_code;
 
-    let api_str = api_key.to_string();
+    let lat;
+    let long;
 
-    let geo_url = format!(
-        "http://api.openweathermap.org/geo/1.0/direct?q={city_code},{province_code},{country_code}&limit=1&appid={api_str}"
-    );
+    let mut wtr_url = String::new();
 
-    let geo_response = blocking::get(&geo_url)?;
-    let geo_content = geo_response.text()?;
+    if let Some(area) = &args.area {
+        if area.len() >= 3 {
+            country_code = &area[0];
+            province_code = &area[1];
+            city_code = &area[2];
+            println!("Args specified");
 
-    let geo_v: Value = serde_json::from_str(&geo_content)?;
+            let geo_url = format!(
+                "http://api.openweathermap.org/geo/1.0/direct?q={city_code},{province_code},{country_code}&limit=1&appid={api_str}"
+            );
 
-    let lat = geo_v[0]["lat"].as_f64().unwrap_or(0.0);
-    let lon = geo_v[0]["lon"].as_f64().unwrap_or(0.0);
+            let geo_response = blocking::get(&geo_url)?;
+            let geo_content = geo_response.text()?;
 
-    let wtr_url = format!(
-        "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_str}"
-    );
+            let geo_v: Value = serde_json::from_str(&geo_content)?;
+
+            let f_lat = geo_v[0]["lat"].as_f64().unwrap_or(0.0);
+            let f_lon = geo_v[0]["lon"].as_f64().unwrap_or(0.0);
+
+            wtr_url = format!(
+                "https://api.openweathermap.org/data/2.5/weather?lat={f_lat}&lon={f_lon}&appid={api_str}"
+            )
+        }
+    } else {
+        let ip_str = reqwest::blocking::get("https://api.ipify.org")
+            .unwrap()
+            .text()
+            .unwrap();
+
+        let area_info = geolocation::find(&ip_str).unwrap();
+
+        long = &area_info.longitude;
+        lat = &area_info.latitude;
+
+        wtr_url = format!(
+            "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_str}"
+        );
+    }
 
     let wtr_response = blocking::get(&wtr_url)?;
     let wtr_content = wtr_response.text()?;
